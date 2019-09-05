93ff65522535759a02f5cf541a39aeb2f24cd5ef
Haneet Singh <singh.haneet007@gmail.com>
2019-09-05 19:53:43 +0530

    Added Enzyme

--- src/App.js
+++ src/App.js
@@ -2,6 +2,6 @@ import React, { Component } from 'react'
 
 export default class App extends Component {
   render() {
-    return (<div className="hello">Hello</div>)
+    return (<div className="hello">Hello, world!</div>)
   }
 }
